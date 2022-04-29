fc0b63cb7346919071a34636b7114ce6524152bb
Gustavo Farias <gustavocel82@gmail.com>
2022-04-29 14:58:12 -0300

    html title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { useState } from "react";
 
 import { AppHeader } from "../components/AppHeader";
@@ -10,17 +11,22 @@ const Home: NextPage = () => {
   const [timeline, setTimeline] = useState<Project[]>();
 
   return (
-    <section className="bg-zinc-800 min-h-screen h-full flex flex-col items-center gap-8">
-      <div className="pt-5">
-        <AppHeader />
-      </div>
-      <div>
-        <Search setTimeline={setTimeline} />
-      </div>
-      <div>
-        <SearchResult timeline={timeline} />
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>Github timeline</title>
+      </Head>
+      <section className="bg-zinc-800 min-h-screen h-full flex flex-col items-center gap-8">
+        <div className="pt-5">
+          <AppHeader />
+        </div>
+        <div>
+          <Search setTimeline={setTimeline} />
+        </div>
+        <div>
+          <SearchResult timeline={timeline} />
+        </div>
+      </section>
+    </>
   );
 };
 
