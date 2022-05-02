757ba34858e1e8a8d5b89a45dd169de84699c874
Gustavo Farias <gustavocel82@gmail.com>
2022-05-02 11:18:37 -0300

    smooth scroll

--- services/fetchProjects.ts
+++ services/fetchProjects.ts
@@ -8,6 +8,7 @@ export async function fetchProjects(username: string): Promise<ProjectFetch> {
   try {
     const req = await fetch(`https://api.github.com/users/${username}/repos`);
     if (req.status === 404) return { isError: true, data: [] };
+
     result.data = await req.json();
   } catch (err) {
     result.isError = true;
